06c8cc80954db73662d3fb601fa584b021ade068
Headev <31333238+headdev@users.noreply.github.com>
2023-08-05 01:57:33 -0500

    creacion de schema y sincronizacion con amazon

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "lotusapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "lotusapp2ee2f779": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
