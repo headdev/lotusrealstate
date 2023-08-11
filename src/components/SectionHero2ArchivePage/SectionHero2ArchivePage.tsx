0438fa1f3cb450834bd778b0e976326707891222
Headev <31333238+headdev@users.noreply.github.com>
2023-08-10 23:31:22 -0500

    cambios a pagina de experiencia

--- src/components/SectionHero2ArchivePage/SectionHero2ArchivePage.tsx
+++ src/components/SectionHero2ArchivePage/SectionHero2ArchivePage.tsx
@@ -33,8 +33,8 @@ const SectionHero2ArchivePage: FC<Props> = ({
     >
       <div className="flex flex-col lg:flex-row lg:items-center">
         <div className="flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-6 lg:space-y-10 pb-14 lg:pb-64 xl:pb-80 xl:pr-14 lg:mr-10 xl:mr-0">
-          <h2 className="font-medium text-4xl md:text-5xl xl:text-7xl leading-[110%]">
-            Tokyo, Jappan
+          <h2 className="font-medium text-4xl md:text-5xl xl:text-5xl leading-[90%]">
+           Encuentra la casa de tus sueños
           </h2>
           <div className="flex items-center text-base md:text-lg text-neutral-500 dark:text-neutral-400">
             <i className="text-2xl las la-map-marked"></i>
