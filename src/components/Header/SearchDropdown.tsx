7d25393f79010474ccd6cc5c0bc4988e290b96e3
Headev <31333238+headdev@users.noreply.github.com>
2023-08-12 13:26:41 -0500

    icono de busqueda

--- src/components/Header/SearchDropdown.tsx
+++ src/components/Header/SearchDropdown.tsx
@@ -39,7 +39,7 @@ const SearchDropdown = () => {
                     <Input
                       ref={inputRef}
                       type="search"
-                      placeholder="Type and press enter"
+                      placeholder="Busca tu propoiedad Aqui "
                     />
                     <input type="submit" hidden value="" />
                   </form>
