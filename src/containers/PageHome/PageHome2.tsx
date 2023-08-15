a5f81c9a086128092d9c9d55133ff79c0cf3d783
Headev <31333238+headdev@users.noreply.github.com>
2023-08-15 15:17:24 -0500

    cambios al home3

--- src/containers/PageHome/PageHome2.tsx
+++ src/containers/PageHome/PageHome2.tsx
@@ -13,21 +13,21 @@ import Heading from "components/Heading/Heading";
 
 
 //
-import logo1 from "images/logos/nomal/1.png";
-import logo1Dark from "images/logos/dark/1.png";
+import logo1 from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-7.jpg";
+import logo1Dark from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-7.jpg";
 //
-import logo2 from "images/logos/nomal/2.png";
-import logo2Dark from "images/logos/dark/2.png";
+import logo2 from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-6.jpg";
+import logo2Dark from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-6.jpg";
 //
-import logo3 from "images/logos/nomal/3.png";
-import logo3Dark from "images/logos/dark/3.png";
+import logo3 from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-5.jpg";
+import logo3Dark from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-5.jpg";
 //
-import logo4 from "images/logos/nomal/4.png";
-import logo4Dark from "images/logos/dark/4.png";
+import logo4 from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-3 (1).jpg";
+import logo4Dark from "images/logos/nomal/aliados_Mesa-de-trabajo-1-copia-3 (1).jpg";
 //
-import logo5 from "images/logos/nomal/5.png";
-import logo5Dark from "images/logos/dark/5.png";
 //
+//
+
 import HIW1img from "images/HIW2-1.png";
 import HIW2img from "images/HIW2-2.png";
 import HIW3img from "images/HIW2-3.png";
@@ -152,7 +152,7 @@ function PageHome2({currentPage}: PageHome2Props) {
       </Heading>
     {/*generar imagenes logos }
         {/* SECTION 1 */}
-        <div className="ncSectionLogos grid grid-cols-3 lg:grid-cols-5 gap-5 sm:gap-16">
+        <div className="ncSectionLogos grid grid-cols-3 lg:grid-cols-4 gap-5 sm:gap-16">
           <div className="flex items-end justify-center">
             <img className="block dark:hidden" src={logo1} alt="logo1" />
             <img className="hidden dark:block" src={logo1Dark} alt="logo1" />
@@ -168,11 +168,6 @@ function PageHome2({currentPage}: PageHome2Props) {
           <div className="flex items-end justify-center">
             <img className="block dark:hidden" src={logo3} alt="logo3" />
             <img className="hidden dark:block" src={logo3Dark} alt="logo3" />
-          </div>
-
-          <div className="flex items-end justify-center">
-            <img className="block dark:hidden" src={logo5} alt="logo5" />
-            <img className="hidden dark:block" src={logo5Dark} alt="logo5" />
           </div>
         </div>
 
